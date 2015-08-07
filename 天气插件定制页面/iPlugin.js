baf1df245581046a5d49dd14c7b841115db92878
JunioJser <407313696@qq.com>
2015-08-07 10:30:17 +0800

    work

--- 天气插件定制页面/iPlugin.js
+++ 天气插件定制页面/iPlugin.js
@@ -1,21 +1,158 @@
 
 ;(function(){
-	//默认配置
-	var config = {name:'未命名', skin:'skin1', city:'北京', width:'200px', height:'100px'};
-	var width = 200, height = 100;
+	//默认iframe
+	var iframe;
+	//样例数据
+	var data = {
+		"plugins": [{
+			"url": "111",
+			"name": "天气实况",
+			"thumbnail": "a.jpg",
+			"skins": [{
+				"name": "扁平风",
+				"value": 0
+			}, {
+				"name": "3D版",
+				"value": 1
+			}],
+			"size":"155,145"
+		},{
+			"url": "111",
+			"name": "天气实况1",
+			"thumbnail": "a.jpg1",
+			"skins": [{
+				"name": "扁平风1",
+				"value": 0
+			}, {
+				"name": "3D版1",
+				"value": 1
+			}],
+			"size":"150,140"
+		},{
+			"url": "111",
+			"name": "天气实况",
+			"thumbnail": "a.jpg",
+			"skins": [{
+				"name": "扁平风",
+				"value": 0
+			}, {
+				"name": "3D版",
+				"value": 1
+			}],
+			"size":"155,145"
+		},{
+			"url": "111",
+			"name": "天气实况1",
+			"thumbnail": "a.jpg1",
+			"skins": [{
+				"name": "扁平风1",
+				"value": 0
+			}, {
+				"name": "3D版1",
+				"value": 1
+			}],
+			"size":"150,140"
+		},{
+			"url": "111",
+			"name": "天气实况",
+			"thumbnail": "a.jpg",
+			"skins": [{
+				"name": "扁平风",
+				"value": 0
+			}, {
+				"name": "3D版",
+				"value": 1
+			}],
+			"size":"155,145"
+		},{
+			"url": "111",
+			"name": "天气实况1",
+			"thumbnail": "a.jpg1",
+			"skins": [{
+				"name": "扁平风1",
+				"value": 0
+			}, {
+				"name": "3D版1",
+				"value": 1
+			}],
+			"size":"150,140"
+		},{
+			"url": "111",
+			"name": "天气实况",
+			"thumbnail": "a.jpg",
+			"skins": [{
+				"name": "扁平风",
+				"value": 0
+			}, {
+				"name": "3D版",
+				"value": 1
+			}],
+			"size":"155,145"
+		},{
+			"url": "111",
+			"name": "天气实况1",
+			"thumbnail": "a.jpg1",
+			"skins": [{
+				"name": "扁平风1",
+				"value": 0
+			}, {
+				"name": "3D版1",
+				"value": 1
+			}],
+			"size":"150,140"
+		}]
+	};
+	var plugins = data.plugins;
+	var skins,width,height,size,baseUrl;
+
+	//初始化插件
+	function iPluginInit(){
+		var div = "<div class='pluginBox'></div>";
+		var a;
+		for(var i=0, len=plugins.length; i<len; i++){
+			if( i%4 == 0 ){
+				$('.pluginContainer .title').after(div);
+			}
+			a = "<a pluginid="+ i +" style='background:" + plugins[i].thumbnail +"' class='plugin' href='javascript:;''><i class='arrow'></i><span>"+ plugins[i].name +"</span></a>";
+			$('.pluginContainer .title').next().append(a);
+		}
+		//初始化
+		$('.pluginContainer').find('.pluginBox').eq(0).children().eq(0).click();
+	};
+
+	//初始化插件对应样式
+	function styleInit(id){
+		//skin
+		skins = plugins[id].skins;
+		size = plugins[id].size.split(',');
+		width = size[0];
+		height = size[1];
+		baseUrl = plugins[id].url;
+		var checkbox;
+		$('#skin label').remove();
+		for(var i = 0, len = skins.length; i<len; i++){
+			checkbox = "<label><input type='radio' name='skin' checked='checked' value='"+ skins[i].value +"'>"+ skins[i].name +"</label>";
+			$('#skin span').after(checkbox);
+		}
+
+		//size
+		$('#width').val(width);
+		$('#height').val(height);
+
+		createCode();
+	};
+
 	//生成代码	
 	function createCode(){
-		config.name = $('#name').val();
-		config.skin = $("input[name='skin']:checked").val();
-		config.city = $('#city').val();
-		config.width = width+'px';
-		config.height = height+'px';
-		var tpl = __inline('/page/testPlugin.tpl'); 
-		content = template.compile(tpl)(config);
-		$('#code').text(content);
-		$('.preLoad').html(content);
-		console.log(config);
-		console.log(content);
+		var w, h, s, c;
+		s = $("input[type='radio']:checked").val();
+		c = $("select").val();
+		w = $('#width').val();
+		h = $('#height').val();
+		var src = baseUrl+'?skin='+s+'&station='+c;
+		iframe = "<iframe src='"+src+"' width='"+ w +"' height='"+ h +"'></iframe>"; 
+		$('#code').text(iframe);
+		$('.preLoad').html(iframe);
 	}
 	//判断是否为数字
 	function isNum(n, def, selector){
@@ -43,32 +180,37 @@
 		$('.pluginBox').toggle();
 		$('.selected').show();
 	});
-	$('.plugin').click(function(){
-		$('.plugin').removeClass('clicked').find('span').text('点击选择');
-		$(this).addClass('clicked').find('span').text('当前选择');
+
+	$('.pluginContainer').on('click', 'a', function(){
+		$('.plugin').removeClass('clicked').find('span');
+		$(this).addClass('clicked').find('span');
 		$('.pluginBox').removeClass('selected').hide();
 		$(this).parent().addClass('selected').show();
-		createCode();
+		var id = $(this).attr('pluginid');
+		styleInit(id);
 	});
 	//样式改变则代码改变
-	$("input[type='radio']").click(function(){
+	$('.styleContainer').on('click', $("input[type='radio']"), function(){
 		createCode();
 	});
-	$("input[type='text']").blur(function(){
-		$();
-		width = $('#width').val(), height = $('#height').val();
-		width = isNum(width, 200, $('#width'));
-		height = isNum(height, 100, $('#height'));
-		console.log(width);
+	$('.styleContainer').on('blur', "input[type='text']", function(){
 		createCode();
 	});
+
 	$('#city').change(function(){
 		createCode();
 	});
+
 	$('.codeBox p').click(function(){
 		var text = $('#code').text();
 		copyToClipboard(text);
 	});
-	//初始化
-	$('.clicked').click();
+
+	$('.size p').click(function(){
+		console.log(1);
+		$('#width').val(width);
+		$('#height').val(height);
+	});
+
+	iPluginInit();
 })();
--- 天气插件定制页面/testPlugin.tpl
+++
@@ -1,4 +0,0 @@
-<div class='plugin1 <%=skin%>' name='<%=name%>' style='height:<%=height%>;width:<%=width%>;'>
-	<p class='city'><%=city%></p>
-	<p>测试</p>
-</div>
