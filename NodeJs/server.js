1b384333220266e01d02c60cb955429ca266a859
JunioJser <407313696@qq.com>
2015-09-25 11:47:05 +0800

    echart的初次使用
    echart的初次使用：地图扩展+折线图+外部联动，遇到的问题，一、无法通过代码对图表实现点击等事件，只能通过修改数据重绘；二、地图上的区域，点击之后只能获取name，期待能给区域加上id

--- NodeJs/server.js
+++ NodeJs/server.js
@@ -6,11 +6,14 @@ function start(route, handle){
 	function onRequest(request,response){
 		var pathname = url.parse(request.url).pathname;
 		console.log("Request for "+pathname+" received.");
+		request.addListener('data', function(chunk){
+			console.log("received chunk");
+		});
 		route(handle, pathname, response, request);
 	}
 
-	//创建一个http服务器并监听8083端口
-	http.createServer(onRequest).listen(8083);
+	//创建一个http服务器并监听8088端口
+	http.createServer(onRequest).listen(8088);
 	console.log("Server has started!");
 }
 //导出模块
